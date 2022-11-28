831cff19dbca6a1fac7ed597cd1a935896eeab96
Jameslilja <89815334+Jameslilja@users.noreply.github.com>
2022-11-28 22:31:19 +0100

    fixade sista delen, var bara previous och next button.

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var arr = ["NatureOne.jpg","NatureTwo.jpg","NatureThree.jpg","NatureFour.jpg"];
 var i = 0;
 next.addEventListener('click',function(){
     i++;
+    if(i > arr.length - 1){
+        i = 0;
+    }
     document.getElementById('image').src = arr[i];
 
 
@@ -16,6 +19,9 @@ next.addEventListener('click',function(){
 
 prev.addEventListener('click',function(){
     i--;
+    if (i < 0){
+        i = arr.length - 1;
+    }
     document.getElementById('image').src = arr[i];
 
 
